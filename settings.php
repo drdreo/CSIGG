660f9d785dfa0df57b9ac6cbead7f3b1e7a2b325
sarahschauppenlehner <s.schauppenlehner@gmx.at>
2016-11-16 17:02:18 +0100

    settings

---
+++ settings.php
@@ -0,0 +1,187 @@
+<?php
+
+require_once 'includes/defines.inc.php';
+require_once NORMPATH . 'session.inc.php';
+require_once TNORMFORM;
+require_once DBACCESS;
+
+
+/**
+ * Class Shop implementiert die Startseite (index.php) des OnlineShop
+ *
+ * Die Seite index.php setzt auf der ojectorientieren Klasse TNormform und den Smarty-Templates von IMAR aus HM2 auf.
+ * Weiters benötigt es die Klasse DBAccess für Datenbankzugriffe, die die Klasse FileAccess von IMAR ersetzt.
+ * Durch die Verwendung von PDO Prepared Statements sind keine weiteren Maßnahmen gegen SQL-Injection notwendig.
+ *
+ * Diese Seite listet die Produkte des OnlineShops in einer Liste auf, die durchblättert werden kann.
+ * Über die Konstante DISPLAY (@see includes/defines.inc.php) wird gesteuert, wieviele Produkte pro Seite angezeigt werden.
+ * Über ein Suchfeld kann ein GET-Request abgesetzt werden, der die Anzahl der Treffer einschränkt.
+ * Über Shop::addToCart() können Produkte über einen POST-Request in den Warenkorb Tabelle onlineshop.cart gelegt werden.
+ *
+ * Die Klasse ist final, da es keinen Sinn macht, davon noch weitere Klassen abzuleiten.
+ *
+ * @author Martin Harrer <martin.harrer@fh-hagenberg.at>
+ * @package onlineshop
+ * @version 2016
+ */
+
+final class Settings extends TNormForm {
+
+    /**
+     * @var string $dbAccess Datenbankhandler für den Datenbankzugriff
+     */
+    private $dbAccess;
+
+    private $firstname;
+    private $lastname;
+    private $username;
+    private $email;
+    private $password;
+    private $uid;
+    /**
+     * Shop Constructor.
+     *
+     * Ruft den Constructor der Klasse TNormform auf.
+     * Erzeugt den Datenbankhandler mit der Datenbankverbindung
+     * Die übergebenen Konstanten finden sich in includes/defines.inc.php
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->dbAccess = new DBAccess(DSN, DB_USER, DB_PWD, DB_NAMES, DB_COLLATION);
+
+    }
+
+    /**
+     * Weist die Inhalte der Smarty-Variablen zu. @see templates/indexMain.tpl
+     *
+     * Gibt die Namen der AddToCart input-Buttons <input type='submit' name='' ... > und damit die Keynamen des Array $_POST vor.
+     * Die Namen der GET-Parameter für das Suchfeld und für die Blätterfunktion und damit die Keynamen im Array $_GET werden ebenfalls vorgegeben.
+     *
+     * Das Array pageArray wird mit allen Einträge der Tabelle onlineshop.product_category befüllt.
+     * Der Aufruf von Shop::fillpageArray() berechnet auch die Variablen für die Blätterfunktion $this->pagecount, pagenumber, current_page, startprevious und startnext.
+     * Diese werden an dieser Stelle nur noch an die Klasse Smarty weitergegeben.
+     * Die Variable $this->search für die Vorbelegung des Suchbegriffs wird ebenfalls von Shop::fillpageArray() befüllt und hier an die Klasse Smarty übergeben.
+     *
+     * Abstracte Methode in der Klasse TNormform und muss daher hier implementiert werden
+     */
+    protected function prepareFormFields() {
+
+        $this->fillPage();
+        $this->smarty->assign('firstname', $this->firstname);
+        $this->smarty->assign('lastname', $this->lastname);
+        $this->smarty->assign('username', $this->username);
+        $this->smarty->assign('email', $this->email);
+        $this->smarty->assign('password', $this->password);
+    }
+
+    /**
+     * Zeigt die Seite mittels Smarty Templates an
+     *
+     * Abstracte Methode in der Klasse TNormform und muss daher hier implementiert werden
+     */
+    protected function display() {
+
+        $this->smarty->display('settings.tpl');
+    }
+
+    /**
+     * Validiert den Benutzerinput nach dem Abschicken einer Bestellung durch einen der Buttons AddToCart.
+     *
+     * Auch wenn die pids im Template durch das PHP-Script index.php eingetragen werden, muss der Input als Benutzerinput gewertet werden,
+     * weil man nicht weiß, ob der Nutzer zum Senden der Bestellung den Request mit entsprechenden Tools noch manipuliert.
+     * Für jede pid ist ein eigener Button implementiert, daher wird jede pid im Array $_POST['pid'] in einem eigenen Eintrag gespeichert.
+     * Mittels Shop::isValidPid() wird jede pid im Array $_POST['pid'] auf positives Integer oder 0 geprüft und damit XSS verhindert.
+     * Zusätzlich wird jede pid geprüft, od diese in der Tabelle onlineshop.product vorkommt, um Forced Browsing mit sinnlosen pids zu verhindern.
+     *
+     * Fehlermeldungen werden im Array $errMsg[] gesammelt.
+     *
+     * Abstracte Methode in der Klasse TNormform und muss daher hier implementiert werden
+     *
+     * @return bool true, wenn $errMsg leer ist. Ansonsten false
+     */
+    protected function isValid() {
+
+
+        return (count($this->errMsg) === 0);
+    }
+
+    /**
+     * Verarbeitet die Benutzereingaben, die mit POST geschickt wurden
+     *
+     * Das Suchfeld wird mit GET übergeben und daher nicht hier behandelt.
+     * Die AddToCart - Buttons sind in ein POST-Formular verpackt nicht als Hyperlinks mit GET-Parametern.
+     * Das ist abhängig vom Shopsystem, wie das in der Praxis gehandhabt wird. Es gibt POST und GET-Implementierungen.
+     * Wir nehmen POST, weil wir die TNormformabläufe schon kennen, die auf POST ausgelegt sind.
+     *
+     * Über Shop::addToCart() wird das ausgewählte Produkt in den Warenkorb Tabelle onlineshop.cart gespeichert.
+     * Im Gutfall wird in $this->statusMsg eine Rückmeldung gegeben, welches Produkt in den Warenkorb gelegt wurde.
+     *
+     * Abstracte Methode in der Klasse TNormform und muss daher hier implementiert werden
+     *
+     * @throws DatabaseException wird von allen $this->dbAccess Methoden geworfen und hier nicht behandelt.
+     *         Die Exception wird daher nochmals weitergereicht (throw) und erst am Ende des Scripts behandelt.
+     */
+    protected function process() {
+        return true;
+        $this->changeUser();
+        $this->statusMsg = "User $this->uid changed successfully";
+    }
+
+    /**
+     * Befüllt das Array um alle Produkte aufzulisten, die auf der aktuellen Seite angezeigt werden.
+     *
+     * Es werden nur aktive Produkte berücksichtigt, bei denen die Spalte onlineshop.product.active='1' ist.
+     *
+     * Es werden nur die Produkte gelesen, die dem Suchkriterium entsprechen. Das Suchkriterium aus dem Suchfeld (GET-Formular) kann leer sein.
+     * Dann gibt es keine Einschränkung.
+     * Suchfelder über die LIKE-Klausel sind die Spalten onlineshop.product_name, .short_description, long_description.
+     * Der Suchbegriff wird in @see Shop::setSearch() ermittelt.
+     *
+     * Der Startwert der LIMIT-Klausel wird in @see Shop::setPaginationParameters() ermittelt.
+     *
+     * Weiters werden die Datensätze in der mittels $_GET[self::SORT] geschickten Sortierreihenfolge ausgegeben (ORDER BY).
+     * Die Sortierreihenfolge wird durch @see Shop::setOrderBy() ermittelt.
+     *
+     * Es werden so viele Sätze gelesen, wie in der Konstante DISPLAY festgelegt. @see includes/defines.inc.php
+     *
+     * @throws DatabaseException Diese wird von allen $this->dbAccess Methoden geworfen und hier nicht behandelt.
+     *         Die Exception wird daher nochmals weitergereicht (throw) und erst am Ende des Scripts behandelt.
+     */
+    private function fillPage() {
+
+        $sql_query = <<< SQL
+        SELECT *
+        FROM
+        USER
+        WHERE iduser = :uid
+SQL;
+//        TODO
+//        Connect to database and fill user data
+        $this->firstname ="";
+        $this->lastname="";
+        $this->username="";
+        $this->email="";
+        $this->password="";
+    }
+
+    private function changeUser() {
+
+        return true;
+
+    }
+}
+/**
+ * Instantiieren der Klasse Shop und Aufruf der Methode TNormform::normForm()
+ *
+ * Datenbank-Exceptions werden erst hier abgefangen und eine formatierte DEBUG-Seite mit den Fehlermeldungen mit echo ausgegeben @see DBAcess::dbugSQL()
+ * Bei PHP-Exception wird vorerst nur auf eine allgemeine Errorpage weitergeleitet
+ */
+try {
+    $settings = new Settings();
+    $settings->normForm();
+} catch (DatabaseException $e) {
+    echo $e->getMessage();
+}
+catch (Exception $e) {
+    echo $e;
+}
---
+++ templates/settings.tpl
@@ -0,0 +1,118 @@
+{include file="{$smarty.const.TNTEMPLATEPATH}header.tpl"}  
+{include file="{$smarty.const.TNTEMPLATEPATH}navbar.tpl"}
+
+<div class="content-wrapper" style="min-height: 670px;">
+    <div class="content-header col-md-12">
+
+        <h1><i class="fa fa-envelope"></i> Contact</h1>
+    </div>
+
+
+    <div class="col-md-12">
+        <div class="col-md-8">
+            <div class="box no-border shadow">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Impressum</h3>
+                </div>
+
+                <div class="box-body">
+                    <div class="table-responsive">
+                        <table class="table">
+
+                            <th colspan="2" class="info">Things you need to know about us.</th>
+
+                            <tr>
+                                <td>created by</td>
+                                <td>Andreas Hahn <br> Sarah Schauppenlehner</td>
+                            </tr>
+                            <tr>
+                                <td>Start Date</td>
+                                <td>16. Nov 2016</td>
+                            </tr>
+                            <tr>
+                                <td>Motivation</td>
+                                <td>We are highly motivated to make the life of the Students easier.</td>
+                            </tr>
+                            <tr>
+                                <td>Impressum</td>
+                                <td>CSIGG is a modern Website which offers a CheatSheet and an Infographic Generator.
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Address</td>
+                                <td>Softwarepark 41/6 <br> 4232 Hagenberg im Mühlkreis <br> Austria</td>
+                            </tr>
+                            <tr>
+                                <td>Telephone Number</td>
+                                <td>0642 9474793</td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>andreas@fh-hagenberg.at <br>sarah@fh-hagenberg.at</td>
+                            </tr>
+                            <tr>
+                                <td>Fax</td>
+                                <td>who the fuck still use this?</td>
+                            </tr>
+
+
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-md-4">
+            <div class="box no-border shadow">
+                <div class="box-header with-border">
+                    <h2 class="box-title">Follow us:</h2>
+                    <div class="box-body">
+                        <a class="btn btn-social-icon btn-primary"><i class="fa fa-facebook"></i></a>
+                        <a class="btn btn-social-icon btn-info"><i class="fa fa-twitter"></i></a>
+                        <a class="btn btn-social-icon btn-primary"><i class="fa fa-instagram"></i></a>
+
+                    </div>
+
+                </div>
+                <div class="box-header with-border">
+                    <h2 class="box-title">Always up to date?</h2>
+
+                    <form>
+                        <div class="box-body form-group">
+
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+
+                        </div>
+                        <div class="box-footer">
+                            <button type="submit" class=" pull-right btn btn-sm btn-flat">Order Newsletter</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+
+            <div class="box no-border shadow">
+                <div class="box-header with-border">
+                    <h2 class="box-title">Questions? <small>Feel free to contact us: </small></h2>
+                    <div class="box-body">
+
+                        <div>
+                            <div class="form-group">
+                                <label for=betreff">Betreff:</label>
+                                <input class="form-control" id ="betreff">
+                                <label for="comment">Message:</label>
+                                <textarea class="form-control" rows="4" id="comment"></textarea>
+                                <a href="mailto:andreas@fh-hagenberg.at"><button type="button" class="btn btn-sm btn-flat pull-right">Send</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+{include file="{$smarty.const.TNTEMPLATEPATH}footer.tpl"}
